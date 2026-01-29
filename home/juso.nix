b2463791fcab0164929c93e8157747498a3c00ba
Geordie Mac <Git@JAJM2006.uk>
2026-01-29 19:23:01 +0000

    Add flake-ready skeleton for UI apps (Niri/Waybar/Dunst/Rofi)

--- home/juso.nix
+++ home/juso.nix
@@ -19,7 +19,21 @@
     direnv
     eza
     starship
+    alacritty
+    rofi
+    waybar
+    dunst
+    grim
+    slurp
+    brightnessctl
+    playerctl
+    nerd-fonts.fira-code
   ];
+
+  home.file.".config/niri".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Settings/config/niri";
+  home.file.".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Settings/config/waybar";
+  home.file.".config/dunst".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Settings/config/dunst";
+  home.file.".config/rofi".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Settings/config/rofi";
 
   programs.btop.enable = true;
   programs.fzf.enable = true;
