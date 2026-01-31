9ab5c960008c44c61345a7ddd9e2bfdd69f29d71
Geordie Mac <Joshua.McManus2006@gmail.com>
2026-01-31 17:49:14 +0000

    v0.3

--- home/juso.nix
+++ home/juso.nix
@@ -115,7 +115,7 @@
   # ZSH 
   # ----------------------------------------------------------------------------
   programs.zsh = {
-    initExtra = ''
+    initContent = ''
       # Case-insensitive completion
       zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
       
